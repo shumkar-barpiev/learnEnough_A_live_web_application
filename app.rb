3587a5e4765d9879781c1a1ce17405c7c8d97ce4
shumkar-barpiev <barpievsh@gmail.com>
2023-06-05 12:39:06 +0600

    embedded ruby

--- app.rb
+++ app.rb
@@ -1,13 +1,16 @@
 require 'sinatra'
 
 get '/' do
+  @title = 'Home'
   erb :index, :layout => :page
 end
 
 get '/about' do
+  @title = 'About'
   erb :about, :layout => :page
 end
 
 get '/palindrome' do
+  @title = 'Palindrome'
   erb :palindrome, :layout => :page
 end
